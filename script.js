e8fedb2bb59a962553f0b86813e7404fe04fa163
sarahsalik08-glitch <sarah.salik08@gmail.com>
2026-06-17 03:08:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-// typing effect
-const text = "Sarah Salik 💖";
+const text = "Hi, I'm Sarah 👩‍💻";
 let i = 0;
 
 function type(){
 if(i < text.length){
 document.querySelector(".typing").innerHTML += text.charAt(i);
 i++;
-setTimeout(type,100);
+setTimeout(type,80);
 }
 }
 
 type();
-
-// floating cursor glow
-const cursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove",(e)=>{
-cursor.style.left = e.clientX + "px";
-cursor.style.top = e.clientY + "px";
-});
